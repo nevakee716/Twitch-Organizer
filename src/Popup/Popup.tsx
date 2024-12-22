9392701ead3b2436a8549dbe9eef847e21828a6f
MPFA1987 <145363593+MPFA1987@users.noreply.github.com>
2024-12-22 14:07:45 +0100

    fix popout behaviours

--- src/Popup/Popup.tsx
+++ src/Popup/Popup.tsx
@@ -1,32 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
-import { Streams } from "./Pages/Streams";
 import browser from "webextension-polyfill";
 
 const Popup = () => {
-  const [whatToLoad, setWhatToLoad] = useState("");
+  browser.tabs.create({
+    url: browser?.runtime?.getURL("/src/Streams/Streams.html"),
+  });
 
-  const analyzeUrl = async () => {
-    let tabs = await browser.tabs.query({ currentWindow: true, active: true });
-    let tab = tabs[0];
-
-    if (tab.url?.includes("twitch.tv")) {
-      setWhatToLoad("streams");
-    } else if (tab.url?.includes("about:newtab") || true) {
-      browser.tabs.create({ url: browser?.runtime?.getURL("Streams.html") });
-    }
-  };
-
-  useEffect(() => {
-    analyzeUrl();
-  }, []);
-
-  switch (whatToLoad) {
-    case "streams":
-      return <Streams />;
-    default:
-      return <>Nothing to load</>;
-  }
+  return null;
 };
 
 createRoot(document.body).render(
