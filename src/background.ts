0f49d9a9a99bc646ba270ebf0beffa07058473e3
MPFA1987 <145363593+MPFA1987@users.noreply.github.com>
2024-12-23 12:08:39 +0100

    adding badge with number of online stream

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,5 @@
 import browser from "webextension-polyfill";
-
+import { useStreamStore } from "@/Streams/Stores/useStore";
 console.log("Hello from the background!");
 
 // Code de développement qui sera exclu en production
@@ -15,3 +15,11 @@ if (import.meta.env.DEV) {
     url: browser.runtime.getURL("src/Streams/Streams.html"),
   });
 }
+
+setInterval(async () => {
+  await useStreamStore.getState().refreshOnlineStatus();
+  const bookmarkStreams = useStreamStore.getState().bookmarkAndFilteredStreams;
+  const onlineCount = bookmarkStreams?.onlineCount || 0;
+  console.log("onlineCount", onlineCount);
+  browser.browserAction.setBadgeText({ text: onlineCount.toString() || "0" });
+}, 60 * 1000);
--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,6 @@ function generateManifest() {
     description: pkg.description,
     version: pkg.version,
     ...manifest,
-    manifest_version: 2,
-    content_security_policy:
-      "script-src 'self' 'wasm-unsafe-eval'; object-src 'self'",
   };
 }
 
@@ -26,7 +23,7 @@ export default defineConfig({
     react(),
     webExtension({
       manifest: generateManifest,
-      browser: "firefox",
+      browser: process.env.TARGET || "firefox",
       webExtConfig: {
         startUrl: ["about:debugging#/runtime/this-firefox", "about:addons"],
         dev: {
@@ -42,11 +39,12 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: process.env.BUILD_PATH || "dist",
     rollupOptions: {
       external: [
         // Exclure les fichiers de dev de la compilation en production uniquement
-        ...(isProd ? [/src\/config\/dev\.config\.ts$/] : [])
-      ]
-    }
-  }
+        ...(isProd ? [/src\/config\/dev\.config\.ts$/] : []),
+      ],
+    },
+  },
 });
